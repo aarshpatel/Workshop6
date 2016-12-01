977e16d7da6a2dca243ae1ac82e6e7ff235f286e
Aarsh Patel <aarshpatel@umass.edu>
2016-11-30 21:21:12 -0500

    database

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,5 @@
-var util = require('./util.js');
-var reverseString = util.reverseString;
-
 // Imports the express Node module.
 var express = require('express');
-var bodyParser = require('body-parser')
-
 
 // Creates an Express server.
 var app = express();
@@ -15,4 +10,5 @@ app.use(express.static('../client/build'));
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
-console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
+});
